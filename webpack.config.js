d7067d4a3c8261b05cff002d42d58bb1278cf99a
Andrea Colla <andcolla97@gmail.com>
2017-11-04 13:23:01 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 var webpack = require('webpack');
 var path = require('path');
+var glob_entries = require('webpack-glob-entries');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: glob_entries(APP_DIR + '/*.jsx'),
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module : {
     loaders : [
